e18ebb2e517dac781e12a961892b191818c4dbe8
Eren <eren.yilmaz@uwaterloo.ca>
2023-04-10 13:05:42 -0400

    final changes

---
+++ Refresh.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+set "script=AndonMonitorSystem.py"
+set "script_path=%~dp0\%script%"
+
+set "excel_file=AndonReport.xlsx"
+set "excel_path=%~dp0\%excel_file%"
+
+echo "Closing Excel..."
+taskkill /f /im excel.exe >nul
+
+echo "Starting refresh..."
+python "%script_path%"
+
+echo "Opening Excel..."
+start "" "%excel_path%"
+
+endlocal
